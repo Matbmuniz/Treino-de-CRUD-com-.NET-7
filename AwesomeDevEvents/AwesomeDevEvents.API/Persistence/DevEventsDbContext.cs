d40cbe4c0506013ff7579fe56a0490ee9a68e830
Matheus Muniz <matheus.barreto@e-construmarket.com.br>
2023-02-26 16:43:53 -0300

    Initial

---
+++ AwesomeDevEvents/AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeDevEvents.API.Enitties;
+
+namespace AwesomeDevEvents.API.Persistence
+{
+	public class DevEventsDbContext
+	{
+		public List<DevEvent> DevEvents { get; set; }
+		public DevEventsDbContext()
+		{
+			DevEvents = new List<DevEvent>();
+		}
+	}
+}
